b5240295c90564530c27528f8b55350b9616e6fd
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-03 20:42:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,18 @@
 class Application
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
+
   def call(env)
-    @@items = []
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.price
+    else
+      resp.write "Item not found"
+      resp.status = 404
+    end
+    resp.finish
   end
 end
