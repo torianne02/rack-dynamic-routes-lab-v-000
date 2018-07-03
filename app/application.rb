01960a04ba584d39aedbccf5b54fd3062e2baa31
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-03 20:44:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+      if @@items.include?(item)
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
