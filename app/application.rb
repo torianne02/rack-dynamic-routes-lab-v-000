ce17cd422e12a3da34ddf74b4e62486cd47d6340
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-03 20:42:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
     else
-      resp.write "Item not found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
